6b91a1c476f9d63c73eca4f9df14eee80eb33568
morrxy <morrxy@gmail.com>
2014-05-29 09:44:40 +0800

    add Nat

--- src/main/scala/week4/List.scala
+++ src/main/scala/week4/List.scala
@@ -1,8 +1,5 @@
 package week4
 
-/**
- * Created by hj on 5/19/14.
- */
 trait List[T] {
   def isEmpty: Boolean
   def head: T
@@ -17,4 +14,11 @@ class Nil[T] extends List[T] {
   def isEmpty: Boolean = true
   def head: Nothing = throw  new NoSuchElementException("Nil.head")
   def tail: Nothing = throw  new NoSuchElementException("Nil.tail")
+}
+
+object List {
+//  List(1, 2) = List.apply(1, 2)
+  def apply[T](x1: T, x2: T): List[T] = new Cons[T](x1, new Cons[T](x2, new Nil))
+  def apply[T](): List[T] = new Nil
+  def apply[T](x: T): List[T] = new Cons[T](x, new Nil)
 }

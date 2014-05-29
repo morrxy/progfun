f77ef8447c30e01dd775d460d6ffa6b5b635ff6e
morrxy <morrxy@gmail.com>
2014-05-29 16:11:54 +0800

    m

--- src/main/scala/week4/List.scala
+++ src/main/scala/week4/List.scala
@@ -1,24 +1,32 @@
 package week4
 
-trait List[T] {
+trait List[+T] {
   def isEmpty: Boolean
   def head: T
   def tail: List[T]
+
+//  def prepend(elem: T): List[T] = new Cons(elem, this)
+  def prepend[U >: T](elem: U):List[U] = new Cons(elem, this)
 }
 
 class Cons[T](val head: T, val tail: List[T]) extends List[T] {
   def isEmpty = false
 }
 
-class Nil[T] extends List[T] {
+object Nil extends List[Nothing] {
   def isEmpty: Boolean = true
   def head: Nothing = throw  new NoSuchElementException("Nil.head")
   def tail: Nothing = throw  new NoSuchElementException("Nil.tail")
 }
 
+object test {
+  val x: List[String] = Nil
+  def f(xs: List[NonEmpty], x: Empty) = xs prepend x
+}
+
 object List {
 //  List(1, 2) = List.apply(1, 2)
-  def apply[T](x1: T, x2: T): List[T] = new Cons[T](x1, new Cons[T](x2, new Nil))
-  def apply[T](): List[T] = new Nil
-  def apply[T](x: T): List[T] = new Cons[T](x, new Nil)
+  def apply[T](x1: T, x2: T): List[T] = new Cons[T](x1, new Cons[T](x2, Nil))
+  def apply[T](): List[T] = Nil
+  def apply[T](x: T): List[T] = new Cons[T](x, Nil)
 }
